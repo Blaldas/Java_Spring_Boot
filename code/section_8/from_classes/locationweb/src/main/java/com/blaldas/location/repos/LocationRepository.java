a45eb367e5a970cc8a0c686d1881aa30e34c5d7a
Blaldas <marmingues00@gmail.com>
2022-09-06 19:01:16 +0100

    added section 8 - 3rd party microservices

---
+++ code/section_8/from_classes/locationweb/src/main/java/com/blaldas/location/repos/LocationRepository.java
@@ -0,0 +1,14 @@
+package com.blaldas.location.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.blaldas.location.entities.Location;
+
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+	@Query(value ="SELECT type, count(type)FROM location GROUP BY type", nativeQuery = true)
+	public List<Object[]> findTypeAndTypeCount();
+}
